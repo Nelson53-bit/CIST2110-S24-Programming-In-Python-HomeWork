0b155becb3bef5779cf423f5b6949a1f0fc8f297
Nelson53-bit <nelson53@go.stockton.edu>
2024-02-13 19:56:45 -0500

    finished hw1

--- HW1.py
+++ HW1.py
@@ -3,70 +3,74 @@
 
 # Question 1:
 # Print Hello World like we did in class
-
+print("Hello World")
 # Question 2:
 # Print the following:
 # Your name
-
+print("Jesse nelson")
 # Your age
-
+print(25)
 # Your favorite color
-
+print("Blue")
 # Your favorite animal
-
+print("Tiger")
 # Question 3:
 # Create a variable called "myName" and set it equal to your name
-
+myName = "Jesse Nelson"
 # Create a variable called "myAge" and set it equal to your age
-
+myAge = "25"
 # Create a variable called "myColor" and set it equal to your favorite color
-
+myColor = "Blue"
 # Create a variable called "myAnimal" and set it equal to your favorite animal
-
+myAnimal = "Tiger"
 # Print the following:
 # Hello, my name is <myName>
 # I am <myAge> years old
 # My favorite color is <myColor>
 # My favorite animal is <myAnimal>
-
+print("Hello my name is ", myName," I am ", myAge, "years old. My favorite color is", myColor, "and My favorite animal is a", myAnimal)
 
 # Question 4:
-# Calculate the following and print the result:
-# 2 + 2
-
+# Calculate the following and print )the result:
+# 2 + 2 
+print( 2 + 2)
 # 3 * 4
-
+print(3 * 4)
 # 5 - 6
-
+print(5 - 6)
 # 8 / 2
-
+print(8 / 2)
 # Question 5:
 # Create a variable called "num1" and set it equal to 2
-
+num1 = 2
 # Create a variable called "num2" and set it equal to 3
-
+num2 = 3
 # Create a variable called "num3" and set it equal to 4
-
+num3 = 4
 # Create a variable called "num4" and set it equal to 5
-
+num4 = 5
 # Calculate the following and print the result:
 # num1 + num2
-
+print(num1 + num2)
 # num3 * num4
-
+print(num3 * num4)
 # num4 - num1
-
+print(num4 - num1)
 # num2 / num1
-
+print(num2 / num1)
 # Question 6: Write a program that asks the user for their name and then prints the following:
 
 # Hello, <name>. Please enter three numbers.
-
+print("What is you're name?")
+user_name = input()
+print("Hello", user_name, "please enter three numbers")
 # The program should then ask the user for three numbers (floats) and print the following:
-
-
+print(user_name,"please give three numbers")
+num1 = input()
+num2 = input()
+num3 = input()
 # 1. The sum of the three numbers is <sum>
-
+print(num1, num2, num3 = sum)  
 # 2. The product of the three numbers is <product>
 
 # 3. round the three numbers to the nearest integer and print the sum of the three rounded numbers divided by 3
@@ -84,3 +88,4 @@
 #  *****   |
 #   ***    |
 #    *     |
+print( )
